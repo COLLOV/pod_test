175dd505d75da6da2422f177214014f7112fb916
COLLOV <collov.j@gmail.com>
2025-02-03 00:35:31 +0100

    nn

--- script.py
+++ script.py
@@ -108,4 +108,4 @@ def creer_image(largeur, hauteur, couleur, nom_fichier):
 
 # Exemple d'utilisation
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=7860)
